fb426452e9a62714f165f3909fdf99ddb88e0c88
Шевченко Александр <ogyrecheg@yandex.ru>
2023-07-22 22:17:43 +0300

    add annotations, docstrings

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,32 +1,39 @@
 import csv
 import datetime as dt
 from collections import defaultdict
+from typing import Dict, List, Tuple, Union
 
+from scrapy import item
 from scrapy.exceptions import DropItem
 
-from .constants import BASE_DIR, DATETIME_FORMAT, FILE_NAME
+from .constants import BASE_DIR, DATETIME_FORMAT, FILE_NAME, OUTPUT_DIR
 
 
 class PepParsePipeline:
-    def open_spider(self, spider):
-        self.status_sum = defaultdict(int)
-        self.start_time = dt.datetime.now().strftime(DATETIME_FORMAT)
+    def open_spider(self, spider) -> None:
+        """Фиксируем время старта паука."""
 
-    def process_item(self, item, spider):
-        if item['status']:
-            self.status_sum[item['status']] += 1
-        else:
+        self.status_sum: Dict[str, int] = defaultdict(int)
+        self.start_time: str = dt.datetime.now().strftime(DATETIME_FORMAT)
+
+    def process_item(self, item: item, spider) -> item:
+        """Подсчитываем количество статусов."""
+
+        if not item['status']:
             raise DropItem('Статус отсутствует!')
+        self.status_sum[item['status']] += 1
 
         return item
 
-    def close_spider(self, spider):
-        results = [('Cтатус', 'Количество')]
+    def close_spider(self, spider) -> None:
+        """Формируем csv файл с результатами парсинга."""
+
+        results: List[Tuple[str, Union[str, int]]] = [('Cтатус', 'Количество')]
         for status in self.status_sum:
             results.append((status, self.status_sum[status]))
         results.append(('Total', sum(self.status_sum.values())))
 
-        results_dir = BASE_DIR / 'results'
+        results_dir = BASE_DIR / OUTPUT_DIR
         results_dir.mkdir(exist_ok=True)
         file_name = f'{FILE_NAME}_{self.start_time}.csv'
         file_path = results_dir / file_name
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,12 +1,14 @@
+from .constants import OUTPUT_DIR
+
+
 BOT_NAME = 'pep_parse'
 
 SPIDER_MODULES = ['pep_parse.spiders']
-NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 ROBOTSTXT_OBEY = True
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    f'{OUTPUT_DIR}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
